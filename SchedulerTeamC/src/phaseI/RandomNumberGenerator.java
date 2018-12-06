a11ce86c236c80b5510a7c77d3d1e67a9f26a3a7
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2018-12-06 13:47:53 -0500

    Random Number Generator
    Done but not tested
    Project outline included with key

--- SchedulerTeamC/src/phaseI/RandomNumberGenerator.java
+++ SchedulerTeamC/src/phaseI/RandomNumberGenerator.java
@@ -1,5 +1,42 @@
 package phaseI;
 
-public class RandomNumberGenerator {
+import java.util.Random;
 
+public class RandomNumberGenerator {
+	Random rand = new Random();
+	
+	//Random Number within a given Range
+	public double random(int floor, int ceiling) {
+		return rand.nextInt((ceiling+1) - floor) + floor;
+	}
+	
+	//Returns a random number between 0 and 1
+	public double random(){
+		return Math.random();
+	}
+	
+	//Returns a random number with a non-uniform distribution
+	public double random(String key) {
+		if (key == "workII") {
+			return nonUniform(200, 600, 5/3);
+		}
+		if (key == "workIII") {
+			return nonUniform(150, 500, 5/2);
+		}
+		if (key == "processIII") {
+			return nonUniform(25, 75, 7/3);
+		}
+		if (key == "lengthC") {
+			return nonUniform(100, 400, 2);
+		}
+		else {
+			return -1;
+		}
+	}
+	
+	public double nonUniform(int low, int high, int frac) {
+		return Math.ceil((low + (high - low)* Math.pow(random(), frac)));
+	}
+	
+	
 }
