c8350564b4762c3c6920c2ede8e92a96384cc34f
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2018-12-06 23:42:16 -0500

    more
    more

--- SchedulerTeamC/src/phaseI/Type4.java
+++ SchedulerTeamC/src/phaseI/Type4.java
@@ -2,6 +2,8 @@ package phaseI;
 
 public class Type4 extends Process {
 	
+	int interval = RandomNumberGenerator.random(40, 120);
+	
 	public Type4(int priority) {
 		super(priority);
 	}
@@ -15,7 +17,16 @@ public class Type4 extends Process {
 	}
 
 	public void generateBlocks() { 
-		 
+		int start = RandomNumberGenerator.random(0, interval);
+		for (int i = start; i< work; i+=interval) {
+			double resource = RandomNumberGenerator.random();
+			if (resource < 0.5) { 
+				blockList.add(new BlockB(i));
+			}
+			else {
+				blockList.add(new BlockC(i));
+			}
+		}
 	}
 
 }
